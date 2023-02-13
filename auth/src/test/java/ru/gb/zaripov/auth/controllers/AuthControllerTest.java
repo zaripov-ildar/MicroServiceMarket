4a0402dc17ffda2e0bd098819b2a30641491a908
ildar <zariil@yandex.ru>
2023-02-13 21:01:00 +0500

    homework 8: user service test & auth controller test are ready

---
+++ auth/src/test/java/ru/gb/zaripov/auth/controllers/AuthControllerTest.java
@@ -0,0 +1,66 @@
+package ru.gb.zaripov.auth.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import ru.gb.zaripov.auth.AdditionalMethods;
+import ru.gb.zaripov.auth.repositories.UserRepository;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
+class AuthControllerTest {
+    private final String USERNAME = "bob";
+    private final String CORRECT_PASSWORD = "correct password";
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private UserRepository userRepository;
+
+
+    @BeforeEach
+    public void setUp() {
+        AdditionalMethods.initUserRepository(userRepository, USERNAME, CORRECT_PASSWORD, "USER");
+    }
+
+
+    @Test
+    void getTokenOnGoodRequest() throws Exception {
+        check(USERNAME, CORRECT_PASSWORD, status().isOk());
+    }
+
+    @Test
+    void getUnauthorizedOnWrongPassword() throws Exception {
+        check(USERNAME, "WRONG_PASSWORD", status().isUnauthorized());
+    }
+
+    @Test
+    void getUnauthorizedOnWrongUserName() throws Exception {
+        check("Wrong user name", CORRECT_PASSWORD, status().isUnauthorized());
+    }
+
+    private void check(String username, String password, ResultMatcher resultMatcher) throws Exception {
+        mockMvc
+                .perform(
+                        post("/api/v1/auth")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(String.format(
+                                        "{\"username\" : \"%s\",\"password\" :\"%s\"}", username, password)
+                                )
+                )
+                .andDo(print())
+                .andExpect(resultMatcher);
+    }
+}
---
+++ auth/src/test/java/ru/gb/zaripov/auth/services/UserServiceTest.java
@@ -0,0 +1,74 @@
+package ru.gb.zaripov.auth.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.opentest4j.AssertionFailedError;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.gb.zaripov.auth.AdditionalMethods;
+import ru.gb.zaripov.auth.entities.Role;
+import ru.gb.zaripov.auth.entities.User;
+import ru.gb.zaripov.auth.repositories.UserRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ru.gb.zaripov.auth.AdditionalMethods.initUserRepository;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @MockBean
+    private UserRepository userRepository;
+    private User user;
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        String USERNAME = "bob";
+        String CORRECT_PASSWORD = "correct password";
+        user = initUserRepository(userRepository, USERNAME, CORRECT_PASSWORD, "USER");
+        userService = new UserService(userRepository);
+    }
+
+    @Test
+    void getDetailsByName() {
+        UserDetails actual = userService.getDetailsByName(user.getUsername());
+        assertEquals(user.getUsername(), actual.getUsername());
+        assertEquals(user.getPassword(), actual.getPassword());
+        assertCollectionEquals(
+                user.getRoles().stream().map(Role::getName).toList(),
+                actual.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList());
+    }
+
+    @Test
+    void getPasswordByName() {
+        UserDetails actual = userService.getDetailsByName(user.getUsername());
+        assertEquals(user.getPassword(), actual.getPassword());
+    }
+
+    @Test
+    void getUserNotFoundException1() {
+        assertThrows(UsernameNotFoundException.class, () -> userService.getDetailsByName("wrong name"));
+    }
+
+    @Test
+    void getUserNotFoundException2() {
+        assertThrows(UsernameNotFoundException.class, () -> userService.getPasswordByName("wrong name"));
+    }
+
+
+    private void assertCollectionEquals(List<String> expected, List<String> actual) {
+        if (expected == null ||
+                actual == null ||
+                !expected.containsAll(actual) ||
+                !actual.containsAll(expected)) {
+            throw new AssertionFailedError("Lists are not equal!");
+        }
+    }
+}
